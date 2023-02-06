f4f454f92dff977d38034e7a3cc9b9aad9c8add6
David Li <davidli012345@gmail.com>
2023-02-06 05:52:22 +0000

    fix: build svelte app

---
+++ src/routes/Base64Encode.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+  
+    export let inputString = '';
+    let encodedString = '';
+  
+    function handleEncode() {
+      encodedString = btoa(inputString);
+    }
+  
+    function handleCopy() {
+      navigator.clipboard.writeText(encodedString);
+    }
+  
+    onMount(() => {
+      console.log('Component mounted.');
+    });
+  </script>
+  
+  <input bind:value={inputString} />
+  <button on:click={handleEncode}>Encode</button>
+  <br />
+  <p>Encoded String: {encodedString}</p>
+  <button on:click={handleCopy}>Copy</button>
